e59ae6b218bd643a8b7ae677a53105291d2e1a97
Shashvat Mehta <68447474+smehtacoder@users.noreply.github.com>
2023-05-14 13:36:30 -0700

    Robot can now move forward for 3 seconds then stops

---
+++ README.md
@@ -0,0 +1,11 @@
+# Python Code for Controlling Osooyo Robot Car
+
+## Installation
+
+- Step 1: Create a Conda Enviornment
+
+- Step 2: Upload pyFirmata sketch on your Arduino
+
+- Step 3: Install pyFirmata, numpy in your environment
+
+- Step 4: Run the test
--- pyfirmata_test.py
+++ pyfirmata_test.py
@@ -1,24 +1,125 @@
 from pyfirmata import ArduinoMega
 import time
 
-board = ArduinoMega("/dev/cu.usbmodem14201")
+board = ArduinoMega("/dev/cu.usbmodem14101")
+
+#List of the pin number for every value (Forward, Reverse, Speed) of each motor
+
+pin_dict = {}
+
+pin_dict["MEGA"] = {"FR_forward": 22,
+                       "FR_reverse": 24,
+                       "FR_speed": 9,
+                       "FL_forward": 26,
+                       "FL_reverse": 28,
+                       "FL_speed": 10,
+                       "BR_forward": 5,
+                       "BR_reverse": 6,
+                       "BR_speed": 11,
+                       "BL_forward": 7,
+                       "BL_reverse": 8,
+                       "BL_speed" : 12}
+
+pin_dict["UNO"] = {"LeftSide_speed": 9,
+                   "LeftSide_forward": 11,
+                   "LeftSide_reverse": 12,
+                   "RightSide_speed": 6,
+                   "RightSide_forward": 8,
+                   "RightSide_reverse": 7}
+
+def FRmotor(robot_type: str, direction: bool, speed:float):
+    if (robot_type == 'MEGA'): 
+        motorForward = board.digital[pin_dict["MEGA"].get("FR_forward")] # one digitial output that controls direction
+        motorReverse = board.digital[pin_dict["MEGA"].get("FR_reverse")] # second digital output that controls direction
+        motorSpeed = board.digital[pin_dict["MEGA"].get("FR_speed")] # analog output that controls speed
+
+        motorSpeed.mode = 3
+        motorForward.mode = 1
+        motorReverse.mode = 1
+
+        if (direction == True) :
+            motorForward.write(1)
+            motorReverse.write(0)
+            
+        elif (direction == False):
+            motorForward.write(0)
+            motorReverse.write(1)
+        
+        motorSpeed.write(speed)
+
+def FLmotor(robot_type: str, direction: bool, speed:float):
+    if (robot_type == 'MEGA'): 
+        motorForward = board.digital[pin_dict["MEGA"].get("FL_forward")] # one digitial output that controls direction
+        motorReverse = board.digital[pin_dict["MEGA"].get("FL_reverse")] # second digital output that controls direction
+        motorSpeed = board.digital[pin_dict["MEGA"].get("FL_speed")] # analog output that controls speed
+
+        motorSpeed.mode = 3
+        motorForward.mode = 1
+        motorReverse.mode = 1
+
+        if (direction == True) :
+            motorForward.write(1)
+            motorReverse.write(0)
+            
+        elif (direction == False):
+            motorForward.write(0)
+            motorReverse.write(1)
+        
+        motorSpeed.write(speed)
+        
+def BRmotor(robot_type: str, direction: bool, speed:float):
+    if (robot_type == 'MEGA'): 
+        motorForward = board.digital[pin_dict["MEGA"].get("BR_forward")] # one digitial output that controls direction
+        motorReverse = board.digital[pin_dict["MEGA"].get("BR_reverse")] # second digital output that controls direction
+        motorSpeed = board.digital[pin_dict["MEGA"].get("BR_speed")] # analog output that controls speed
+
+        motorSpeed.mode = 3
+        motorForward.mode = 1
+        motorReverse.mode = 1
+
+        if (direction == True) :
+            motorForward.write(1)
+            motorReverse.write(0)
+            
+        elif (direction == False):
+            motorForward.write(0)
+            motorReverse.write(1)
+        
+        motorSpeed.write(speed)
+        
+def BLmotor(robot_type: str, direction: bool, speed:float):
+    if (robot_type == 'MEGA'): 
+        motorForward = board.digital[pin_dict["MEGA"].get("BL_forward")] # one digitial output that controls direction
+        motorReverse = board.digital[pin_dict["MEGA"].get("BL_reverse")] # second digital output that controls direction
+        motorSpeed = board.digital[pin_dict["MEGA"].get("BL_speed")] # analog output that controls speed
+
+        motorSpeed.mode = 3
+        motorForward.mode = 1
+        motorReverse.mode = 1
+
+        if (direction == True) :
+            motorForward.write(1)
+            motorReverse.write(0)
+            
+        elif (direction == False):
+            motorForward.write(0)
+            motorReverse.write(1)
+        
+        motorSpeed.write(speed)
+        
+def StopMotors():
+    FRmotor("MEGA", True, 0)
+    BRmotor("MEGA", True, 0)
+    FLmotor("MEGA", True, 0)
+    BLmotor("MEGA", True, 0)
 
 
-motorFRpos = board.digital[22] # one digitial output that controls direction
-motorFRneg = board.digital[24] # second digital output that controls direction
-motorFRspeed = board.digital[9] # analog output that controls speed
-
-motorFRspeed.mode = 3
-motorFRpos.mode = 1
-motorFRneg.mode = 1
-
-motorFRpos.write(1)
-motorFRneg.write(0)
-motorFRspeed.write(0.75)
-
+FRmotor("MEGA", True, 1)
+BRmotor("MEGA", True, 1)
+FLmotor("MEGA", True, 1)
+BLmotor("MEGA", True, 1)
 time.sleep(5)
-
-motorFRspeed.write(0)
+StopMotors()
 
 # while True:
 #     board.digital[13].write(1)
