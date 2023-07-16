27a6108fdffea860f31fa8e267f443474d055aac
Shashvat Mehta <68447474+smehtacoder@users.noreply.github.com>
2023-07-16 11:00:57 -0700

    Added keyboard press for servo

--- Exercises/Exercise_2.py
+++ Exercises/Exercise_2.py
@@ -1,16 +1,53 @@
-from pyfirmata import ArduinoMega, SERVO
+from pyfirmata import ArduinoMega, SERVO, OUTPUT
 import time
+import pygame
 
-board = ArduinoMega("/dev/cu.usbmodem14201")
+board = ArduinoMega("/dev/cu.usbmodem14401")
 
-board.digital[13].mode = SERVO
+servoPin = 13
+
+board.digital[servoPin].mode = SERVO
+
+
+#Init pygame and pygame screen
+pygame.init
+screen = pygame.display.set_mode((1000,500), pygame.RESIZABLE)
+pygame.display.set_caption("Servo Control")
+
+GREEN = (79,121,66)
+BLUE = (2,130,242)
 
 def rotateServo(angle):
-    board.digital[13].write(angle)
+    board.digital[servoPin].write(angle)
+
+Run = True
+
+x = 500
+y = 250
+
+while Run:
     
-while True:
-    rotateServo(0)
-    time.sleep(0.5)
-    rotateServo(360)
-    time.sleep(0.5)
-    rotateServo(0)
+    keys = pygame.key.get_pressed()
+    
+    #Moves circle and motor based off keypress
+    if keys[pygame.K_o]:
+        rotateServo(0)
+        pygame.draw.circle(screen, BLUE, (x,y), 25)
+
+
+    if keys[pygame.K_c]:
+        rotateServo(180)
+        pygame.draw.circle(screen, GREEN, (x,y), 25)
+
+    
+    
+    #Quits the game
+    events = pygame.event.get()
+    for event in events:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_ESCAPE:
+                Run = False
+        if event.type == pygame.QUIT:
+            Run = False
+                
+pygame.quit
